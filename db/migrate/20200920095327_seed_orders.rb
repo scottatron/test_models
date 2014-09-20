88c0b42fd4d429ba4767bf3a8a8fafe39b004033
Jon Calhoun <joncalhoun@gmail.com>
2014-09-20 05:57:28 -0400

    adding some migrations to seed data

---
+++ db/migrate/20200920095327_seed_orders.rb
@@ -0,0 +1,31 @@
+class SeedOrders < ActiveRecord::Migration
+  def change
+    (1..200).each do |i|
+      user = User.find(rand(User.count) + 1)
+      order = Order.create!(user: user)
+      num_products = rand(4) + 1
+      (1..num_products).each do |k|
+        quantity = rand(4) + 1
+        product = Product.find(rand(Product.count) + 1)
+        po = ProductOrder.new
+        po.order = order
+        po.product = product
+        po.quantity = quantity
+        po.purchase_price = product.price
+        po.save!
+      end
+      s = order.build_shipment
+      s.price = (rand(1000) / 100.0) + 10.0,
+      s.carrier = [:ups, :usps, :fedex, :dhl][rand(4)],
+      s.delivered = [true, true, false][rand(3)],
+      s.est_delivery_date = random_date
+      s.save!
+  end
+
+  def random_date(ago = 60, from_now = 20)
+    ago = (0..ago).to_a.map{|i| i.days.ago}
+    from_now = (1..from_now).to_a.map{|i| i.days.ago}
+    all = ago + from_now
+    return all[rand(all.length)]
+  end
+end
